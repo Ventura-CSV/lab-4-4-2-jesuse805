a4266640122483ef601de0c3e25b55f8d8babb22
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-27 23:54:39 -0700

    CS V11

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ def main():
     ########################################
     Code Your Program here
     """
+    #Via Slide 25
     while True:
-    
-    
+        user_input = input('')
+        try:
+            number = int(user_input)
+        except ValueError:
+            print('Input must be numeric')
+            continue
+        else:
+            print(number)
+            break
     
     
     """
